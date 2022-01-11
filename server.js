41dac11b7b5715925ab7731970c1ec1ad4e8319f
Shmee <smaheilman@gmail.com>
2022-01-10 21:07:00 -0500

    manifest loading

--- server.js
+++ server.js
@@ -1,22 +1,22 @@
 const express = require("express");
-//const logger = require("morgan");
+const logger = require("morgan");
 const mongoose = require("mongoose");
-//const compression = require("compression");
+const compression = require("compression");
 
 const PORT = process.env.PORT || 3001;
 const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/budget";
 
 const app = express();
 
-//app.use(logger("dev"));
+app.use(logger("dev"));
 
-//app.use(compression());
+app.use(compression());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(MONGODB_URI || 'mongodb://localhost:27017/budget-tracker', {
+mongoose.connect(MONGODB_URI, {
     useNewUrlParser: true,
     //useFindAndModify: false
 });
